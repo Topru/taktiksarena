0fb795c63e838e74778ebf1b64b10b2c256de726
Topias <topias.jappila@gmail.com>
2017-09-28 01:28:12 +0300

    first commit

---
+++ Assets/Explode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+   public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "Terrain")
+        {
+            Debug.Log("collision");
+            explode();
+        }
+    }
+
+    private void explode()
+    {
+        var expl = Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject); // destroy the grenade
+        Destroy(expl, (float)0.5);
+    }
+}
