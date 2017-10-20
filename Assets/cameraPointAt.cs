e0364f8959c282695dcb66f3e6d286fc2cd0ec8e
Topias <topias.jappila@gmail.com>
2017-10-20 19:01:24 +0300

    camera zoom depending on distance

--- Assets/cameraPointAt.cs
+++ Assets/cameraPointAt.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cameraPointAt : MonoBehaviour {
+   
+    public float zoomFactor;
 
 	// Use this for initialization
 	void Start () {
@@ -11,12 +13,23 @@ public class cameraPointAt : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        Camera camera = gameObject.GetComponent<Camera>();
+
         GameObject player1 = GameObject.FindWithTag("Player1");
         GameObject player2 = GameObject.FindWithTag("Player2");
 
         UnityEngine.Vector3 player1loc = player1.transform.position;
         UnityEngine.Vector3 player2loc = player2.transform.position;
 
+        float distance = Vector3.Distance(player1loc, player2loc);
+
+        float cameraFov = 20 + distance * 1.0f;
+        
+        if(cameraFov > 70)
+        {
+            cameraFov = 70;
+        }
+        camera.fieldOfView = cameraFov;
         float cameraLookX = player1loc.x + (player2loc.x - player1loc.x) / 2;
         float cameraLookZ = player1loc.z + (player2loc.z - player1loc.z) / 2;
         float cameraLookY = player1loc.y + (player2loc.y - player1loc.y) / 2;
