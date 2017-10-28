e47051fce184bc17eb10a2419abdaf3f72148c58
Topias <topias.jappila@gmail.com>
2017-10-29 01:39:48 +0300

    explosion damage based on distance

---
+++ Assets/Damage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+    public float damagePerDistance;
+
+	// Use this for initialization
+	void Start () {
+        ApplyDamage();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ApplyDamage()
+    {
+        for (int i = 1; i < 3; i++)
+        {
+            GameObject p = GameObject.FindWithTag("Player" + i);
+            if (p != null)
+            {
+                float distance = Vector3.Distance(p.transform.position, transform.position);
+                if (distance < 3.5f)
+                {
+                    float damage = (3.5f - distance) * damagePerDistance;
+                    int intDamage = (int)damage;
+                    Debug.Log(intDamage);
+                    p.GetComponent<DamageController>().ApplyDamage(intDamage);
+                }
+
+            }
+        }
+    }
+}
