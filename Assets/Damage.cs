fbd85807e6b2af171fdc27bb95d204a8905c4ea8
Topias <topias.jappila@gmail.com>
2017-11-01 01:01:02 +0200

    made explosion damage more consistent

--- Assets/Damage.cs
+++ Assets/Damage.cs
@@ -7,26 +7,28 @@ public class Damage : MonoBehaviour {
     public float radius;
 	// Use this for initialization
 	void Start () {
-        
-	}
+        ApplyDamage();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    void OnParticleCollision(GameObject collision)
+
+    void ApplyDamage()
     {
-        ApplyDamage(collision);
-    }
-    void ApplyDamage(GameObject collision)
-    {
-        GameObject p = collision.gameObject;
-        if(p.tag == "Player1" || p.tag == "Player2") {
-            Vector3 closestPoint = p.GetComponent<Rigidbody>().ClosestPointOnBounds(transform.position);
-            float distance = Vector3.Distance(closestPoint, transform.position);
-            float damage = damagePerDistance * (1.0F - Mathf.Clamp01(distance / radius));
-            int intDamage = (int)damage;
-            p.GetComponent<DamageController>().ApplyDamage(intDamage);
+        for (int i = 1; i < 3; i++)
+        {
+            GameObject p = GameObject.FindWithTag("Player" + i);
+            if (p != null)
+            {
+                Vector3 closestPoint = p.GetComponent<Rigidbody>().ClosestPointOnBounds(transform.position);
+                float distance = Vector3.Distance(closestPoint, transform.position);
+                float damage = damagePerDistance * (1.0F - Mathf.Clamp01(distance / radius));
+                int intDamage = (int)damage;
+                Debug.Log(intDamage);
+                p.GetComponent<DamageController>().ApplyDamage(intDamage);
+            }
         }
     }
 }
