1f38c09147deabc9f5a3f03fcd31d5d5a73ce251
Topias <topias.jappila@gmail.com>
2017-11-09 20:41:06 +0200

    camera follow single player

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject player;
+    public float cameraHeight = 20.0f;
+    public GameController gameController;
+    // Use this for initialization
+    void Start() {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        player = gameController.GetLocalPlayer();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = player.transform.position;
+        pos.y += cameraHeight;
+        transform.position = pos;
+    }
+}
