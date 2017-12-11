6583bd6a1cc304b402e81dc3d823dfab5f36c0d3
Topias <topias.jappila@gmail.com>
2017-12-11 20:56:31 +0200

    camera rotate around

--- Assets/cameraPointAt.cs
+++ Assets/cameraPointAt.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class cameraPointAt : MonoBehaviour {
    
-    public float zoomFactor;
+    public float speed;
 
 	// Use this for initialization
 	void Start () {
@@ -16,28 +16,8 @@ public class cameraPointAt : MonoBehaviour {
 	void Update () {
         Camera camera = gameObject.GetComponent<Camera>();
 
-        GameObject player1 = GameObject.FindWithTag("Player1");
-        GameObject player2 = GameObject.FindWithTag("Player2");
-
-        UnityEngine.Vector3 player1loc = player1.transform.position;
-        UnityEngine.Vector3 player2loc = player2.transform.position;
-
-        float distance = Vector3.Distance(player1loc, player2loc);
-
-
-        float cameraFov = 20f + distance * 1.0f;
-        if(cameraFov > 70)
-        {
-            cameraFov = 70;
-        }
-        camera.fieldOfView = cameraFov;
-        float cameraLookX = player1loc.x + (player2loc.x - player1loc.x) / 2;
-        float cameraLookZ = player1loc.z + (player2loc.z - player1loc.z) / 2;
-        float cameraLookY = player1loc.y + (player2loc.y - player1loc.y) / 2;
-
-
-        camera.transform.position = Vector3.Lerp(player1loc, player2loc, 0.5f) + new Vector3(0f, 130f, 0);
-        gameObject.transform.LookAt(new UnityEngine.Vector3(cameraLookX, cameraLookY, cameraLookZ)+offset);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        gameObject.transform.LookAt(GameObject.Find("Center").transform.position);
         
         //Debug.Log("Player1loc: " + player1loc);
         //Debug.Log("Player2loc: " + player2loc);
